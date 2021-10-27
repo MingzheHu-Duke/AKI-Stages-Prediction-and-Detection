ce89b00898e9f4be65b20e6aedaee51ee6c0a4aa
MingzheHu <58562242+MingzheHu-Duke@users.noreply.github.com>
2021-10-27 06:26:57 -0400

    Create remove_duplicates.sql

---
+++ SQL/remove_duplicates.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `my-project-mimic-iii-330123.MyMIMIC.raw_patient_data_recent`
+DROP COLUMN icustay_id, DROP COLUMN aki_stage_creat, DROP COLUMN aki_stage_uo, DROP COLUMN subject_id, DROP COLUMN hadm_id, DROP COLUMN subject_id_1, DROP COLUMN hadm_id_1, DROP COLUMN icustay_id_2 
